1d175473bd220300f9e4d1af0e4ce79540e0b309
n1nka <sakaev@it-projects.info>
2019-05-16 19:05:07 +0500

    PYLINT_HTML_FOR_ADDONS_DEV_PR_ONLY

--- travis/run_pylint.py
+++ travis/run_pylint.py
@@ -96,14 +96,11 @@ def get_extra_params(odoo_version, disable_pylint=None):
     return extra_params
 
 
-def get_beta_msgs():
-    """Get beta msgs from beta.cfg file
-    :return: List of strings with beta message names"""
+def get_beta_cfg():
     travis_build_dir = os.environ.get('TRAVIS_PULL_REQUEST_SLUG')
-    print(travis_build_dir)
-    print('---------------------------------------TRAVIS_PULL_REQUEST_SLUG')
-    find_addons_dev = re.search(r'addons-dev', travis_build_dir)
-    if find_addons_dev:
+    is_PR = os.environ.get('TRAVIS_PULL_REQUEST')
+    find_addons_dev = re.search(r'addons-dev', str(travis_build_dir))
+    if find_addons_dev and is_PR:
         beta_cfg = os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             'cfg/travis_run_pylint_beta_addons_dev.cfg')
@@ -111,6 +108,13 @@ def get_beta_msgs():
         beta_cfg = os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             'cfg/travis_run_pylint_beta.cfg')
+    return beta_cfg
+
+
+def get_beta_msgs():
+    """Get beta msgs from beta.cfg file
+    :return: List of strings with beta message names"""
+    beta_cfg = get_beta_cfg()
     if not os.path.isfile(beta_cfg):
         return []
     config = ConfigParser.ConfigParser()
