58c4fcd3df2d0a809224ee0500c686890f218522
n1nka <sakaev@it-projects.info>
2019-05-17 18:44:56 +0500

    method readfp

--- travis/run_pylint.py
+++ travis/run_pylint.py
@@ -104,12 +104,21 @@ def get_beta_msgs():
         'cfg/travis_run_pylint_beta.cfg')
     if not os.path.isfile(beta_cfg):
         return []
+    enable_checks_addons_dev_pr = []
     config = ConfigParser.ConfigParser()
     config.readfp(open(beta_cfg))
-    return [
-        msg.strip()
-        for msg in config.get('MESSAGES CONTROL', 'enable').split(',')
-        if msg.strip()]
+    enable_checks = [msg.strip() for msg in config.get('MESSAGES CONTROL', 'enable').split(',') if msg.strip()]
+    travis_pull_request_slug = os.environ.get('TRAVIS_PULL_REQUEST_SLUG')
+    is_PR = os.environ.get('TRAVIS_PULL_REQUEST')
+    find_addons_dev = re.search(r'addons-dev', str(travis_pull_request_slug))
+    if find_addons_dev and is_PR:
+        beta_cfg_addons_dev_html = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'cfg/travis_run_pylint_beta_addons_dev.cfg')
+        config.readfp(open(beta_cfg_addons_dev_html))
+        enable_checks_addons_dev_pr = [msg.strip() for msg in config.get('MESSAGES CONTROL', 'enable').split(',') if msg.strip()]
+    list_enable_checks = enable_checks + enable_checks_addons_dev_pr
+    return list_enable_checks
 
 
 def get_modules_cmd(dir):
