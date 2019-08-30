ac4b4c1333aae405fa25fe26d4284f001cbf992d
kaadevelop <oleg88rubakov@gmail.com>
2019-08-30 16:45:44 +0500

    check tags docs

--- travis/check_tags.py
+++ travis/check_tags.py
@@ -70,10 +70,11 @@ def handler_commit(commit, symbol_in_branch, version, travis_build_dir, travis_r
         errors_commit.update(errors_dev)
     else:
         errors_stable = check_stable_branch_tags(dev_tag, release_tag, commit)
-        errors_stable_docs = check_stable_branch_docs(release_tag, commit, travis_build_dir, travis_repo_slug, travis_pull_request_number, travis_branch)
         errors_commit.update(errors_stable)
-        errors_commit.update(errors_stable_docs)
 
+    errors_stable_docs = check_stable_branch_docs(release_tag, commit, travis_build_dir, travis_repo_slug,
+                                                  travis_pull_request_number, travis_branch)
+    errors_commit.update(errors_stable_docs)
     if any(tag in REQUIREMENTS_TAGS_OF_VERSION for tag in list_tags):
         errors_version = check_version_tags(version_tags, list_tags, commit, version)
         errors_commit.update(errors_version)
@@ -82,8 +83,8 @@ def handler_commit(commit, symbol_in_branch, version, travis_build_dir, travis_r
 
 def check_stable_branch_docs(release_tag, commit, travis_build_dir, travis_repo_slug, travis_pull_request_number, travis_branch):
     errors_stable_docs = {}
-    # modules_changed = get_modules_changed(travis_build_dir, travis_branch)
-    # print('-------------------------modules_changed:\n{}'.format(modules_changed))
+    modules_changed = get_modules_changed(travis_build_dir, travis_branch)
+    print('-------------------------modules_changed:\n{}'.format(modules_changed))
     modules_info = get_modules_info(travis_build_dir)
     print('-------------------------modules_info:\n{}'.format(modules_info))
     return errors_stable_docs
