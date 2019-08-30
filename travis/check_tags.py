4f673d4ed5c8f6bab3d49123dbd9a6a09c9f535b
kaadevelop <oleg88rubakov@gmail.com>
2019-08-30 17:59:35 +0500

    check tags docs

--- travis/check_tags.py
+++ travis/check_tags.py
@@ -20,6 +20,7 @@ def get_errors_msgs_commits(travis_repo_slug, travis_pull_request_number, travis
     #GET /repos/:owner/:repo/pulls/:pull_number/commits
     #See API Github: https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
     real_errors = {}
+    print('22222222222222222222222222222')
     if not travis_pull_request_number or travis_pull_request_number == "false":
         return real_errors
     # GET / repos /: owner /:repo / commits
