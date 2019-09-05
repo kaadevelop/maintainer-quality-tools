e901d5a2301bc861e984c4e54eb66380a43f0c48
kaadevelop <oleg88rubakov@gmail.com>
2019-09-05 20:47:20 +0500

    get version

--- travis/check_tags.py
+++ travis/check_tags.py
@@ -89,10 +89,12 @@ def get_versions_from_manifest(travis_repo_slug, travis_pull_request_number):
         filename = file.get('filename')
         if any(x in filename for x in ['__manifest__.py', 'doc/changelog.rst', 'doc/index.rst']):
             patch_changed_file[filename] = file.get('patch')
+    print('patch_changed_file\n{}'.format(patch_changed_file))
     versions_from_manifest = {}
     for key, value in patch_changed_file:
         if '__manifest__.py' in key:
             versions = re.findall(r'(\d+.\d.\d.\d.\d)\\', value)
+            print('versions {}'.format(versions))
             versions_from_manifest[key] = versions
     return versions_from_manifest
 
