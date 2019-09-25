60477493562df4219a5b2c0a657031135b1fcb1a
kaadevelop <oleg88rubakov@gmail.com>
2019-09-25 14:18:15 +0500

    update

--- travis/check_tags.py
+++ travis/check_tags.py
@@ -288,10 +288,8 @@ def get_changed_version(commit_url, commits_order):
             if 'README.rst' in filename:
                 filename_versions.update({filename: 'Updated!'})
         commit_filename_versions[commit_msg] = filename_versions
+    commit_manifest = {**commits_order, **commit_manifest}
     print('commit_manifest\n{}'.format(commit_manifest))
-    # commit_manifest_sorted = [commit_manifest[k] for k in sorted(commits_order, key=commits_order.get)]
-    commit_manifest_sorted = sorted(commit_manifest.items(), key=lambda x: [commits_order[x] for x in commits_order.keys() if commit_manifest[x] == commit_manifest[x]])
-    print('commit_manifest_sorted is {}'.format(commit_manifest_sorted))
     return commit_filename_versions, commit_manifest
 
 
