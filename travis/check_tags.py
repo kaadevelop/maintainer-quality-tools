013cad2456748f2f1d75b982256c7fdd05d3d727
kaadevelop <oleg88rubakov@gmail.com>
2019-09-27 16:19:51 +0500

    fix updated changelog

--- travis/check_tags.py
+++ travis/check_tags.py
@@ -130,7 +130,7 @@ def get_manifest_version(travis_repo_slug, sha_commits):
     sha_start = sha_commits[0]
     sha_end = sha_commits[-1]
     # GET /repos/:owner/:repo/compare/:base...:head
-    url_request = 'https://github.it-projects.info/repos/%s/compare/%s...%s' % (
+    url_request = 'https://github.it-projects.info/repos/{}/compare/{}...{}'.format(
         str(travis_repo_slug), str(sha_start),  str(sha_end))
     print('url_request\n'.format(url_request))
     resp = requests.get(url_request)
